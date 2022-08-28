f3961c8a26c726eadd568e7dcb9fc6305aad0a6b
blaze-fire <krishans290@gmail.com>
2022-08-28 18:21:35 +0530

    Refactored some code

--- Binary Search/Agressive_Cows.cpp
+++ Binary Search/Agressive_Cows.cpp
@@ -1,49 +1,76 @@
+/*
+ Input:
+
+1
+5 3
+1
+2
+8
+4
+9
+
+Output: 3
+ */
+
 #include<iostream>
+#include<algorithm>
+
 using namespace std;
 
-bool isValid(int stalls[], int n, int cows, int min_sep){
-	int last_cow = stalls[0], count = 1;
-	for(int i=1; i<n; i++){
-		if(stalls[i] - last_cow >= min_sep){
-			count++;
-			last_cow = stalls[i];
-			if(count == cows){
-				return true;
-			}
-		}
-	}
-	
-	return false;
+bool check(int stalls[], int n, int c, int min_sep){
+    int count = 1;
+    int last = stalls[0];
+    for(int i=1; i<n; i++){
+        if(stalls[i] - last >= min_sep){
+            count++;
+
+            if(count == c){
+                return true;
+            }
+
+            last = stalls[i];
+        }
+    }
+
+    return false;
+
 }
 
+
 int main(){
-	
-	
-		int stalls[] = {1, 2, 4, 8, 9};
-        int num_stalls = 5;
-        int cows = 3;
-        
-		
-		int start = 0, end = stalls[num_stalls-1] - stalls[0];
-		int ans = 0;
-			
-		while(start <= end){
-			int mid = (start + end)/2;
-			
-			bool ifCanPlace = isValid(stalls, num_stalls, cows, mid);
-			
-			if(ifCanPlace){
-				ans = mid;
-				start = mid+1;
-			}
-			
-			else{
-				end = mid-1;
-			}
-		}
-		
-		cout<<ans<<endl;
-	
-	
-	return 0;
+
+    int tc;
+    cin>>tc;
+
+    while(tc--){
+        int n,c;
+        cin>>n>>c;
+
+        int stalls[n];
+        for(int i=0; i<n; i++){
+            cin>>stalls[i];
+        }
+
+        sort(stalls, stalls+n);
+
+        int s=0, e=stalls[n-1] - stalls[0], ans = -1;
+
+        while(s <= e){
+            int mid = (s+e)/2;
+            bool canPlace = check(stalls, n, c, mid);
+            if(canPlace){
+                ans = mid;
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+
+        cout<<ans<<endl;
+
+    }
+
+    cout<<endl;
+    return 0;
 }
